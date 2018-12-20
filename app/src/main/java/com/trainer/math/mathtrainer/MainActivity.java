5a3c6e8a990fe4a696bca8314b5cfbeb1a7dd936
yasster <ymissoum1@gmail.com>
2018-12-19 22:37:32 -0800

    Refractored MainActivity

--- app/src/main/java/com/trainer/math/mathtrainer/MainActivity.java
+++ app/src/main/java/com/trainer/math/mathtrainer/MainActivity.java
@@ -8,20 +8,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnSettings = (Button) findViewById(R.id.btn_settings);
+
+        // Assigning button on onCreate
+        btnSettings = (Button) findViewById(R.id.btn_settings);
+
+
+        // Button listener to call start other activity action
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                settingButton();
+                startActivity();
             }
         });
     }
 
-    private void settingButton(){
+    private void startActivity(){
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
