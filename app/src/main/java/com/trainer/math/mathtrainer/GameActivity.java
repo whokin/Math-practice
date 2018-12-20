2f032375ac3929236bf19422068da4f16669501d
Warren Ho kin <warrenhokin@gmail.com>
2018-12-20 06:31:08 -0800

    Added 2 levels of multiplication difficulties

--- app/src/main/java/com/trainer/math/mathtrainer/GameActivity.java
+++ app/src/main/java/com/trainer/math/mathtrainer/GameActivity.java
@@ -11,8 +11,9 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
 
@@ -22,9 +23,16 @@ public class GameActivity extends AppCompatActivity {
     Button btnCheck;
     Button btnShowAnswer;
     Button btnNextQuestion;
+    RadioGroup radioGroup;
+    RadioButton radLevel1;
+    RadioButton radLevel2;
+//    Button btn_level1;
+//    Button btn_level2;
 
     Vibrator vibrator;
 
+    int minBound;
+    int maxBound;
     int randX;
     int randY;
     Equation equation;
@@ -37,6 +45,9 @@ public class GameActivity extends AppCompatActivity {
 
         // Populate references for elements
         populateReference();
+
+        // Sets default level difficulty to 1
+        level1();
 
         // Display the question
         updateQuestion();
@@ -72,9 +83,7 @@ public class GameActivity extends AppCompatActivity {
         btnShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message = "The answer is " + equation.getStrResult();
                 etxtAnswer.setText(equation.getStrResult());
-                Toast.makeText(GameActivity.this, message, Toast.LENGTH_SHORT).show();
                 etxtAnswer.setBackgroundColor(Color.GREEN);
             }
         });
@@ -86,10 +95,42 @@ public class GameActivity extends AppCompatActivity {
                 validateAnswer();
                 if (answerFound)
                     updateQuestion();
-                else
-                    Toast.makeText(GameActivity.this,
-                            "Answer not found yet.",
-                            Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        radLevel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level1();
+            }
+        });
+
+        radLevel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                level2();
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton checkedRadioButton = (RadioButton) group.findViewById(checkedId);
+                boolean isChecked = checkedRadioButton.isChecked();
+                switch (checkedId) {
+                    case R.id.rad_level1:
+                        if (isChecked){
+                            minBound = 2;
+                            maxBound = 12;
+                        }
+                        break;
+                    case R.id.rad_level2:
+                        if (isChecked){
+                            minBound = 2;
+                            maxBound = 100;
+                        }
+                        break;
+                }
             }
         });
     }
@@ -98,18 +139,12 @@ public class GameActivity extends AppCompatActivity {
         // Validates the answer compared to the input field
         equation.multiply();
         if (etxtAnswer.getText().toString().equals(equation.getStrResult())) {
-            Toast.makeText(GameActivity.this,
-                    "Correct!",
-                    Toast.LENGTH_SHORT).show();
             etxtAnswer.setBackgroundColor(Color.GREEN);
             answerFound = true;
         } else {
-            Toast.makeText(GameActivity.this,
-                    "Try again!",
-                    Toast.LENGTH_SHORT).show();
             etxtAnswer.setBackgroundColor(Color.RED);
             answerFound = false;
-            if(vibrator != null && vibrator.hasVibrator()){
+            if (vibrator != null && vibrator.hasVibrator()) {
                 vibrator.vibrate(500);
             }
         }
@@ -130,16 +165,37 @@ public class GameActivity extends AppCompatActivity {
         btnCheck = findViewById(R.id.btn_check);
         btnShowAnswer = findViewById(R.id.btn_show_answer);
         btnNextQuestion = findViewById(R.id.btn_new);
+        radioGroup = findViewById(R.id.radGroup);
+        radLevel1 = findViewById(R.id.rad_level1);
+        radLevel2 = findViewById(R.id.rad_level2);
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
     }
 
     private void updateQuestion() {
         // Randomizes two new integers and updates the question text display
-        randX = new Random().nextInt(12 - 2) + 2;
-        randY = new Random().nextInt(12 - 2) + 2;
+        randX = new Random().nextInt(maxBound - minBound) + minBound;
+        randY = new Random().nextInt(maxBound - minBound) + minBound;
         equation = new Equation(randX, randY);
         questionDisplay();
         etxtAnswer.setText("");
         etxtAnswer.setBackgroundColor(0x00000000);
+    }
+
+    private void level1() {
+        // sets the min and max bound to be 2 and 12 respectively
+        // and colors the currently active level difficulty
+        radLevel1.setBackgroundColor(Color.argb(1, 249, 166, 2));
+        radLevel2.setBackgroundResource(android.R.drawable.btn_default);
+        minBound = 2;
+        maxBound = 12;
+    }
+
+    private void level2() {
+        // sets the min and max bound to be 2 and 100 respectively
+        // and colors the currently active level difficulty
+        radLevel2.setBackgroundColor(Color.argb(1, 249, 166, 2));
+        radLevel1.setBackgroundResource(android.R.drawable.btn_default);
+        minBound = 2;
+        maxBound = 100;
     }
 }
